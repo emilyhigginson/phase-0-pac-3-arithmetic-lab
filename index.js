ffaa08edc016eb5d9312377fe987a5bfbc5f5510
Emily Higginson <emilyhigginson14@gmail.com>
2021-11-30 09:21:36 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a,b) {
+    return (a + b); 
+}
+function subtract (a,b) {
+    return (a-b);
+}
+function multiply(a,b) {
+    return (a*b);
+}
+function divide(a,b) {
+    return (a/b);
+}
+function increment(n){
+    return (n+=1);
+}
+function decrement(n){
+    return (n-=1);
+}
+function add5() {
+    return (n +=5);
+}
+function divideBy3() {
+    return(n /= 3);
+}
+function makeInt(string) {
+    return parseInt(string, 10); 
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+   
+    
+}
